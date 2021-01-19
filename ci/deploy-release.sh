3781687973c0ac5c5ae7ed5610d6a44c6ee0efd3
Jochen Schalanda <jochen@schalanda.name>
2021-01-19 08:10:02 +0100

    Migrate releases to GitHub release workflow (#76)

--- ci/deploy-release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-set -uxo pipefail
-
-# Decrypt and import signing key
-openssl aes-256-cbc -K $encrypted_da5c7df06829_key -iv $encrypted_da5c7df06829_iv -in ci/dropwizard.asc.enc -out ci/dropwizard.asc -d
-gpg --armor --import ci/dropwizard.asc
-
-./mvnw -B deploy --settings 'ci/settings.xml' -DperformRelease=true -Dmaven.test.skip=true
